7e66498a257f44df1cefe8f16a03dcf3e6c78aad
Zsolt Rokolya <rokyhu@gmail.com>
2021-07-27 10:18:27 +0200

    [display] product images

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -24,19 +24,19 @@ public class Initializer implements ServletContextListener {
         SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
 
         //setting up a new supplier
-        Supplier amazon = new Supplier("Amazon", "Digital content and services");
-        supplierDataStore.add(amazon);
-        Supplier lenovo = new Supplier("Lenovo", "Computers");
-        supplierDataStore.add(lenovo);
+        Supplier hackerTees = new Supplier("Hacker Tees", "Hacker Tees for your Geeky Needs");
+        supplierDataStore.add(hackerTees);
+        Supplier coolStuff = new Supplier("Cool Stuff", "Your T-shirts, customized");
+        supplierDataStore.add(coolStuff);
 
         //setting up a new product category
-        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        productCategoryDataStore.add(tablet);
+        ProductCategory tShirt = new ProductCategory("T-Shirt", "Apparel", "A t-shirt commonly shortened to tee, is awesome.");
+        productCategoryDataStore.add(tShirt);
 
         //setting up products and printing it
-        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
-        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
-        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
-        productDataStore.add(new Product("Hello", 122.2f, "USD", "Yeah!", tablet, lenovo));
+        productDataStore.add(new Product("Breaking Builds", 29.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tShirt, hackerTees, "CoolCode_Breaking-Builds.jpeg", "CoolCode_Breaking-Builds_hover.jpeg"));
+        productDataStore.add(new Product("Bug Feature", 19.9f, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tShirt, coolStuff, "CoolCode_Bug-Feature.jpeg", "CoolCode_Bug-Feature_hover.jpeg"));
+        productDataStore.add(new Product("Commit", 23.9f, "USD", "Amazon's latest Fire HD 8 tShirt is a great value for media consumption.", tShirt, hackerTees, "CoolCode_Commit.jpeg", "CoolCode_Commit_hover.jpeg"));
+        productDataStore.add(new Product("Do You Even Unit Test", 27.9f, "USD", "Yeah!", tShirt, coolStuff, "CoolCode_Do-you-even-unit-test.jpeg", "CoolCode_Do-you-even-unit-test_hover.jpeg"));
     }
 }
