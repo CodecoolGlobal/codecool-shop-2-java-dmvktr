be72e60c01c7b67edce103f16a6f44b41386d2f9
viktor <v.dome1991@gmail.com>
2021-07-27 13:47:44 +0200

    [add] fetch and product rebuild on main page

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -4,22 +4,63 @@ const page = {
     categoryEndpointURL: "/getProductsByCategory",
     baseImagePath: "/static/img/product-img/",
 
-    init: function () {
+    init: function (){
         this.initCategoryMenuEventListeners();
     },
 
-    initCategoryMenuEventListeners: function () {
+    initCategoryMenuEventListeners: function (){
         const categories = document.querySelector(".catagories-menu");
         let menuOptions = categories.querySelectorAll('li');
 
-        menuOptions.forEach(o => o.addEventListener('click', () => {
+        menuOptions.forEach(o => o.addEventListener('click', ()=>{
             const URL = `${this.categoryEndpointURL}?id=${o.dataset.categoryId}`;
             dataHandler.fetchData(URL, this.rebuildProducts);
         }))
     },
 
-    rebuildProducts: function (products) {
-        // implement function
+    rebuildProducts: function (products){
+        const container = document.querySelector('#product-container');
+        container.innerHTML = "";
+
+        for (const product of products) {
+            console.log(product);
+            page.rebuildSingleProduct(container, product);
+        }
+    },
+
+    rebuildSingleProduct: function (container, product){
+        container.insertAdjacentHTML('beforeend', `
+        <div class="col-12 col-sm-6 col-md-12 col-xl-6">
+            <div class="single-product-wrapper">
+                <div class="product-img">
+                    <img src="${this.baseImagePath}${product['imagePath']}" alt="">
+                    <img class="hover-img" src="${this.baseImagePath}${product['hoverImagePath']}" alt="">
+                </div>
+                <div class="product-description d-flex align-items-center justify-content-between">
+                    <div class="product-meta-data">
+                        <div class="line"></div>
+                        <p class="lead product-price">${product['defaultPrice']}</p>
+                        <div class="ratings">
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                        </div>
+                        <a href="product-details.html">
+                            <h4 class="card-title">${product['name']}</h4>
+                        </a>
+                        <p class="card-text">${product['description']} </p>
+                    </div>
+                    <div class="ratings-cart text-right" style="min-width: 50px">
+                        <div class="cart">
+                            <a href="cart.html" data-toggle="tooltip" data-placement="left" title="Add to Cart"><img src="/static/img/core-img/cart-purple.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+        )
     }
 }
 
