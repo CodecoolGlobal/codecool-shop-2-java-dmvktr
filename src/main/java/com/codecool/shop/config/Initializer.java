dac81836393ef0707be47393a7e3b97171a22739
n0rbertz <norbz123@gmail.com>
2021-07-27 11:10:59 +0200

    [add] dynamic category display

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -32,11 +32,18 @@ public class Initializer implements ServletContextListener {
         //setting up a new product category
         ProductCategory tShirt = new ProductCategory("T-Shirt", "Apparel", "A t-shirt commonly shortened to tee, is awesome.");
         productCategoryDataStore.add(tShirt);
+        ProductCategory sticker = new ProductCategory("Sticker", "Stationery", "A sticker to your laptop.");
+        productCategoryDataStore.add(sticker);
 
         //setting up products and printing it
         productDataStore.add(new Product("Breaking Builds", 29.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tShirt, hackerTees, "CoolCode_Breaking-Builds.jpeg", "CoolCode_Breaking-Builds_hover.jpeg"));
         productDataStore.add(new Product("Bug Feature", 19.9f, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tShirt, coolStuff, "CoolCode_Bug-Feature.jpeg", "CoolCode_Bug-Feature_hover.jpeg"));
         productDataStore.add(new Product("Commit", 23.9f, "USD", "Amazon's latest Fire HD 8 tShirt is a great value for media consumption.", tShirt, hackerTees, "CoolCode_Commit.jpeg", "CoolCode_Commit_hover.jpeg"));
         productDataStore.add(new Product("Do You Even Unit Test", 27.9f, "USD", "Yeah!", tShirt, coolStuff, "CoolCode_Do-you-even-unit-test.jpeg", "CoolCode_Do-you-even-unit-test_hover.jpeg"));
+        productDataStore.add(new Product("sticker1", 27.9f, "USD", "Yeah!", sticker, coolStuff, "CoolCode_Do-you-even-unit-test.jpeg", "CoolCode_Do-you-even-unit-test_hover.jpeg"));
+        productDataStore.add(new Product("sticker2", 27.9f, "USD", "Yeah!", sticker, coolStuff, "CoolCode_Do-you-even-unit-test.jpeg", "CoolCode_Do-you-even-unit-test_hover.jpeg"));
+        productDataStore.add(new Product("sticker3", 27.9f, "USD", "Yeah!", sticker, coolStuff, "CoolCode_Do-you-even-unit-test.jpeg", "CoolCode_Do-you-even-unit-test_hover.jpeg"));
+        productDataStore.add(new Product("sticker4", 27.9f, "USD", "Yeah!", sticker, coolStuff, "CoolCode_Do-you-even-unit-test.jpeg", "CoolCode_Do-you-even-unit-test_hover.jpeg"));
+
     }
 }
--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -30,6 +30,8 @@ public class ProductController extends HttpServlet {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         context.setVariable("category", productService.getProductCategory(1));
+        context.setVariable("categories", productCategoryDataStore.getAll());
+
         context.setVariable("products", productService.getProductsForCategory(1));
         // // Alternative setting of the template context
         // Map<String, Object> params = new HashMap<>();
