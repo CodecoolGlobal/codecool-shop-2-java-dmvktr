bffddf074611095490fe33860a0f2423148acfe6
viktor <v.dome1991@gmail.com>
2021-07-29 11:23:59 +0200

    [relocate] main page cart functionality to new JS file

---
+++ src/main/webapp/static/js/cart-main-page.js
@@ -0,0 +1,22 @@
+import {dataHandler} from "./data-handler.js";
+
+
+const cart = {
+    userId: 1,
+    updateOrderBaseURL: "/update-order",
+
+    init: function (){
+        this.initCartEventListeners();
+    },
+
+    initCartEventListeners: function (){
+        let carts = document.querySelectorAll('.product-cart');
+
+        carts.forEach(cart => cart.addEventListener('click', ()=>{
+            const URL = `${this.updateOrderBaseURL}?user_id=${this.userId}&product_id=${cart.dataset.productId}&quantity_diff=1`;
+            dataHandler.fetchData(URL, this.logResponse);
+        }))
+    },
+}
+
+cart.init();
