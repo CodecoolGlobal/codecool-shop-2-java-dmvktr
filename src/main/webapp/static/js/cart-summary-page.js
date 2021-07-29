99099876215e1abda82da95a256f9dcd9d397562
viktor <v.dome1991@gmail.com>
2021-07-29 12:11:11 +0200

    [fix] code format

--- src/main/webapp/static/js/cart-summary-page.js
+++ src/main/webapp/static/js/cart-summary-page.js
@@ -7,47 +7,47 @@ const cart = {
     cartSummarySubtotalSpan: document.querySelector('#cart-summary-subtotal'),
     cartSummaryTotalSpan: document.querySelector('#cart-summary-total'),
 
-    init: function (){
+    init: function () {
         this.initCartEventListeners();
         this.displayCartTotalSummary();
         this.initQuantityAdjustmentButtonListeners();
     },
 
-    initCartEventListeners: function (){
+    initCartEventListeners: function () {
         let carts = document.querySelectorAll('.product-cart');
 
-        carts.forEach(cart => cart.addEventListener('click', ()=>{
+        carts.forEach(cart => cart.addEventListener('click', () => {
             const URL = `${this.updateOrderBaseURL}?user_id=${this.userId}&product_id=${cart.dataset.productId}&quantity_diff=1`;
             dataHandler.fetchData(URL, this.logResponse);
         }))
     },
 
-    displayCartTotalSummary: function (){
+    displayCartTotalSummary: function () {
         const subtotalValue = document.querySelector('.subtotal').dataset.subtotal;
         cart.setCartSummaryField(cart.cartSummarySubtotalSpan, subtotalValue);
         cart.setCartSummaryField(cart.cartSummaryTotalSpan, subtotalValue);
     },
 
-    setCartSummaryField: function (fieldName, value){
+    setCartSummaryField: function (fieldName, value) {
         fieldName.innerHTML = value;
     },
 
-    initQuantityAdjustmentButtonListeners: function (){
+    initQuantityAdjustmentButtonListeners: function () {
         const increaseQuantityButtons = document.querySelectorAll('.qty-plus');
         const decreaseQuantityButtons = document.querySelectorAll('.qty-minus');
         increaseQuantityButtons.forEach(button => button.addEventListener('click', this.adjustCartContent))
-        decreaseQuantityButtons.forEach(button=>button.addEventListener('click', this.adjustCartContent))
+        decreaseQuantityButtons.forEach(button => button.addEventListener('click', this.adjustCartContent))
     },
 
-    adjustCartContent: function (evt){
+    adjustCartContent: function (evt) {
         const quantityDiv = evt.target.closest('.quantity');
-        const quantityDiff = evt.currentTarget.classList.contains('qty-plus')? "1":"-1";
+        const quantityDiff = evt.currentTarget.classList.contains('qty-plus') ? "1" : "-1";
         const productId = quantityDiv.dataset.productId;
         const URL = `${cart.updateOrderBaseURL}?user_id=${cart.userId}&product_id=${productId}&quantity_diff=${quantityDiff}`;
         dataHandler.fetchData(URL, cart.logResponse);
-        },
+    },
 
-    logResponse: function (data){
+    logResponse: function (data) {
         console.log(data);
     },
 }
