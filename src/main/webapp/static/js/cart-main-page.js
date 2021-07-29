d475a16402ff87f1a9d9b7d3ed897d86662cb6e7
viktor <v.dome1991@gmail.com>
2021-07-29 12:11:35 +0200

    [fix] cart-main code format

--- src/main/webapp/static/js/cart-main-page.js
+++ src/main/webapp/static/js/cart-main-page.js
@@ -5,17 +5,21 @@ const cart = {
     userId: 1,
     updateOrderBaseURL: "/update-order",
 
-    init: function (){
+    init: function () {
         this.initCartEventListeners();
     },
 
-    initCartEventListeners: function (){
+    initCartEventListeners: function () {
         let carts = document.querySelectorAll('.product-cart');
 
-        carts.forEach(cart => cart.addEventListener('click', ()=>{
+        carts.forEach(cart => cart.addEventListener('click', () => {
             const URL = `${this.updateOrderBaseURL}?user_id=${this.userId}&product_id=${cart.dataset.productId}&quantity_diff=1`;
             dataHandler.fetchData(URL, this.logResponse);
         }))
+    },
+
+    logResponse: function (data) {
+        console.log(data);
     },
 }
 
