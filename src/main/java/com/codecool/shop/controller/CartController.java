36f5439b2147e2a398e1fbe2b958e8c35be2bb90
Zsolt Rokolya <rokyhu@gmail.com>
2021-07-31 22:58:49 +0200

    [refactor] CartController class

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -1,26 +1,19 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
-import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.ProductDao;
-import com.codecool.shop.dao.SupplierDao;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.dao.implementation.SupplierDaoMem;
-import com.codecool.shop.model.LineItem;
+import com.codecool.shop.controller.util.EngineProcessor;
 import com.codecool.shop.model.Order;
-import com.codecool.shop.service.ProductService;
-import com.codecool.shop.service.ProductServiceFactory;
+import com.codecool.shop.service.*;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @WebServlet(urlPatterns = {"/cart"})
@@ -29,14 +22,13 @@ public class CartController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductService productService = ProductServiceFactory.get();
+
+        Map<String, Object> templateVariables = new HashMap<>();
         // TODO remove hardcoded order #1 during 2nd sprint
-        Optional<Order> order = productService.getOrderDao().getBy(1);
-        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-        WebContext context = new WebContext(req, resp, req.getServletContext());
+        templateVariables.put("order", productService.getOrderDao().getBy(1).orElse(null));
 
-        context.setVariable("order", order.orElse(null));
-
-        engine.process("product/cart.html", context, resp.getWriter());
+        String htmlFilename = "product/cart.html";
+        EngineProcessor.apply(req, resp, templateVariables, htmlFilename);
     }
 
 }
