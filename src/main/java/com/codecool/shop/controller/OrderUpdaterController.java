6991d5bca548bef0e4cc2197bca4e92cb392a2fb
Zsolt Rokolya <rokyhu@gmail.com>
2021-07-31 22:59:47 +0200

    [refactor] OrderUpdaterController class

--- src/main/java/com/codecool/shop/controller/OrderUpdaterController.java
+++ src/main/java/com/codecool/shop/controller/OrderUpdaterController.java
@@ -1,28 +1,15 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.ProductDao;
-import com.codecool.shop.dao.SupplierDao;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.controller.util.JsonReturner;
 import com.codecool.shop.model.Order;
-import com.codecool.shop.model.Product;
 import com.codecool.shop.service.ProductService;
 import com.codecool.shop.service.ProductServiceFactory;
-import com.google.gson.Gson;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 @WebServlet(urlPatterns = {"/update-order"})
 public class OrderUpdaterController extends HttpServlet {
@@ -31,19 +18,16 @@ public class OrderUpdaterController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductService productService = ProductServiceFactory.get();
 
-        int userID = Integer.parseInt(req.getParameter("user_id"));
-        int productID = Integer.parseInt(req.getParameter("product_id"));
-        int quantity = Integer.parseInt(req.getParameter("quantity_diff"));
-
-        productService.getOrderDao().handleOrderUpdate(userID, productID, quantity);
+        try {
+            int userID = Integer.parseInt(req.getParameter("user_id"));
+            int productID = Integer.parseInt(req.getParameter("product_id"));
+            int quantity = Integer.parseInt(req.getParameter("quantity_diff"));
+            productService.getOrderDao().handleOrderUpdate(userID, productID, quantity);
+        } catch (NumberFormatException e) {
+            System.out.println("Unable to process product change (please check the query string): " + e);
+        }
 
         Order order = productService.getOrderDao().getBy(1).orElse(null);
-        Gson gson = new Gson();
-        String jsonResponse = gson.toJson(order);
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
-        PrintWriter out = resp.getWriter();
-        out.write(jsonResponse);
-        out.flush();
+        JsonReturner.apply(resp, order);
     }
 }
