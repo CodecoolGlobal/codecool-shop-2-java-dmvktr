8f28584b7738e2cc925ff23c0b72068f30c87527
Zsolt Rokolya <rokyhu@gmail.com>
2021-08-09 14:09:04 +0200

    [add] ProductServiceTest and maven dependencies

---
+++ src/test/java/com/codecool/shop/service/ProductServiceTest.java
@@ -0,0 +1,17 @@
+package com.codecool.shop.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class ProductServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testTest() {
+
+    }
+}
