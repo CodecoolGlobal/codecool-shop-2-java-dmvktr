79f19c6dff7e4a61ba530d0f800c5d2d6e89b015
n0rbertz <norbz123@gmail.com>
2021-08-10 11:01:43 +0200

    [add] SLF4J logger configuration

--- src/main/java/com/codecool/shop/dao/implementation/OrderDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/OrderDaoMem.java
@@ -5,7 +5,11 @@ import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.model.LineItem;
 import com.codecool.shop.model.Order;
 import com.codecool.shop.model.Product;
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +19,7 @@ public class OrderDaoMem implements OrderDao {
     private List<Order> data = new ArrayList<>();
     private static OrderDaoMem instance = null;
     private ProductDao productDataStore = ProductDaoMem.getInstance();
+    final Logger logger = LoggerFactory.getLogger(OrderDaoMem.class);
 
     public static OrderDaoMem getInstance() {
         if (instance == null) {
@@ -31,6 +36,7 @@ public class OrderDaoMem implements OrderDao {
         else {
             Order newOrder = addOrder(userID);
             updateProductQuantityInOrder(newOrder, productDataStore.find(productID), quantityDiff);
+            logger.info(LocalDateTime.now() +  " | User " + userID + " created new order " + newOrder.getOrderID());
         }
     }
 
@@ -78,6 +84,7 @@ public class OrderDaoMem implements OrderDao {
                 item.updateQuantity(quantityDiff);
                 if (item.isQuantityZero()) {
                     order.removeItem(item);
+                    logger.info(LocalDateTime.now() +  " | User " + order.getUserID() + " removed " + product.getName() + " from cart.");
                 }
                 order.refreshTotalPrice();
                 order.refreshItemCount();
