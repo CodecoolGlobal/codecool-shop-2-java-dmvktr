175d5be96ebeedc2d784a49d57495dcc10b16b83
Zsolt Rokolya <rokyhu@gmail.com>
2021-08-11 01:35:14 +0200

    [add] logger for OrderUpdaterController

--- src/main/java/com/codecool/shop/controller/OrderUpdaterController.java
+++ src/main/java/com/codecool/shop/controller/OrderUpdaterController.java
@@ -1,9 +1,12 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.config.AppProperties;
 import com.codecool.shop.controller.util.JsonReturner;
 import com.codecool.shop.dao.OrderDao;
 import com.codecool.shop.dao.implementation.OrderDaoMem;
 import com.codecool.shop.model.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +16,8 @@ import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/update-order"})
 public class OrderUpdaterController extends HttpServlet {
+
+    private static Logger logger = LoggerFactory.getLogger(OrderUpdaterController.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -24,11 +29,10 @@ public class OrderUpdaterController extends HttpServlet {
             int quantity = Integer.parseInt(req.getParameter("quantity_diff"));
             orderDao.handleOrderUpdate(userID, productID, quantity);
         } catch (NumberFormatException e) {
-            System.out.println("Unable to process product change (please check the query string): " + e);
+            logger.warn("Unable to process product change (please check the query string)");
         }
 
         Order order = orderDao.getBy(1).orElse(null);
-        System.out.println(order);
         JsonReturner.apply(resp, order);
     }
 }
