c46914bfe2cb539a139e685b623b639c823c6cd6
viktor <v.dome1991@gmail.com>
2021-08-11 19:14:59 +0200

    [update] productController to reflect changes to session

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -3,26 +3,32 @@ package com.codecool.shop.controller;
 import com.codecool.shop.controller.util.EngineProcessor;
 import com.codecool.shop.dao.OrderDao;
 import com.codecool.shop.dao.implementation.OrderDaoMem;
+import com.codecool.shop.model.Order;
 import com.codecool.shop.service.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @WebServlet(urlPatterns = {"/"})
 public class ProductController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        HttpSession session = req.getSession();
+        Integer userID = (Integer) session.getAttribute("user_id");
         ProductService productService = ProductServiceStore.get();
         OrderDao orderDao = OrderDaoMem.getInstance();
+        Order order = (Order) session.getAttribute("cart");
 
         Map<String, Object> templateVariables = new HashMap<>();
-        templateVariables.put("order", orderDao.getBy(1).orElse(null));
-//        templateVariables.put("category", productService.getProductCategory(2));
+
+        if(userID == null && order == null) templateVariables.put("order", orderDao.addOrder());
+        else if(userID == null) templateVariables.put("order", order);
+        else templateVariables.put("order", orderDao.getBy(userID));
         templateVariables.put("categories", productService.getProductCategoryDao().getAll());
         templateVariables.put("suppliers", productService.getSupplierDao().getAll());
         templateVariables.put("products", productService.getProductsForCategory(1));
