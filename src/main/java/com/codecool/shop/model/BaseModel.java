a8896429aa0e5e82433b628bde797d9930d6920a
viktor <v.dome1991@gmail.com>
2021-08-12 17:24:11 +0200

    [add] equals and hashcode override

--- src/main/java/com/codecool/shop/model/BaseModel.java
+++ src/main/java/com/codecool/shop/model/BaseModel.java
@@ -2,6 +2,7 @@ package com.codecool.shop.model;
 
 
 import java.lang.reflect.Field;
+import java.util.Objects;
 
 public class BaseModel {
 
@@ -64,4 +65,16 @@ public class BaseModel {
         return sb.toString();
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseModel baseModel = (BaseModel) o;
+        return getId() == baseModel.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
