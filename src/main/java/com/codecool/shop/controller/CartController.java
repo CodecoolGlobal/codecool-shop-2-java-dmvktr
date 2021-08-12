ad5f64d07989db8bc84e24255aad63aaa26a2e67
viktor <v.dome1991@gmail.com>
2021-08-12 18:09:02 +0200

    [update] CartController order acquisition

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -1,13 +1,16 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.controller.util.EngineProcessor;
+import com.codecool.shop.controller.util.OrderProvider;
 import com.codecool.shop.dao.OrderDao;
 import com.codecool.shop.dao.implementation.OrderDaoMem;
+import com.codecool.shop.model.Order;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,11 +20,9 @@ public class CartController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        OrderDao orderStore = OrderDaoMem.getInstance();
-
         Map<String, Object> templateVariables = new HashMap<>();
-        // TODO remove hardcoded order #1 during 2nd sprint
-        templateVariables.put("order", orderStore.getBy(1).orElse(null));
+        Order order = OrderProvider.get(req.getSession());
+        templateVariables.put("order", order);
 
         String htmlFilename = "product/cart.html";
         EngineProcessor.apply(req, resp, templateVariables, htmlFilename);
